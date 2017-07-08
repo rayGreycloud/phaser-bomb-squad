42cdfe5928f23fcd3461affad1b1cc47a011106b
rayGreycloud <rayGreycloud@gmail.com>
2017-07-08 07:08:26 -0700

    Load robot and animations

--- js/stateMain.js
+++ js/stateMain.js
@@ -1,11 +1,17 @@
 var StateMain = {
 
     preload: function () {
-
+      game.load.spritesheet('robot', 'assets/images/main/robot.png', 80, 111, 28);
     },
 
     create: function () {
-        console.log("Ready!");
+      this.robot = game.add.sprite(150, 150, 'robot');
+      this.robot.animations.add('idle', [0,1,2,3,4,5,6,7,8,9], 12, true);
+      this.robot.animations.add('walk', [10,11,12,13,14,15,16,17], 12, true);
+      this.robot.animations.add('jump', [18,19, 20,21,22,23,24,25], 12, false);
+
+      this.robot.animations.play('idle');
+
     },
 
     update: function () {
