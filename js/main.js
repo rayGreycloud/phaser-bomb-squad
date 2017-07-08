ff44c07f64dc16813404904170cbef928b57adf5
rayGreycloud <rayGreycloud@gmail.com>
2017-07-08 07:01:50 -0700

    Refactor main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 var game;
-window.onload = function()
-{
-    if (screen.width>1500)
-	{
-      game=new Phaser.Game(480,640,Phaser.AUTO,"ph_game");
-	}
-	else
-	{		
-		game=new Phaser.Game(window.innerWidth,window.innerHeight,Phaser.AUTO,"ph_game");		
-	}
-    game.state.add("StateMain",StateMain);
-    game.state.start("StateMain");
+
+window.onload = function () {
+
+  var width = screen.width > 1500 ? 640 : window.innerWidth;
+  var height = screen.width > 1500 ? 480 : window.innerHeight;
+
+  game = new Phaser.Game(width, height, Phaser.AUTO, 'ph_game');
+  game.state.add('StateMain', StateMain);
+  game.state.start('StateMain');
+  
 }
